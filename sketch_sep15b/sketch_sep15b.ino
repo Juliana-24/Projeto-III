d86bc749044d30e950dc6a97a30f9d93d12b1253
Juliana Dallazen Ribeiro <80290030+Juliana-24@users.noreply.github.com>
2022-11-25 21:33:17 -0400

    Add files via upload

---
+++ sketch_sep15b/sketch_sep15b.ino
@@ -0,0 +1,118 @@
+#include <AFMotor.h>
+
+AF_DCMotor MotorEsquerda(3);
+AF_DCMotor MotorDireita(2);
+
+
+int BTN_W = A0;
+int BTN_A = A1;
+int BTN_D = A5;
+int BTN_E = A4;
+
+String commands = "";
+
+void setup()
+{
+  pinMode(BTN_W, INPUT);
+  pinMode(BTN_A, INPUT);
+  pinMode(BTN_D, INPUT);
+  pinMode(BTN_E, INPUT);
+  
+  Serial.begin(9600);
+
+  Serial.println("Projeto Robo Educacional");
+}
+
+void loop()
+{  
+  if (digitalRead(BTN_W)) {
+    commands.concat("W ");
+    Serial.println(commands);
+    delay(200);
+  }  
+  if (digitalRead(BTN_A)) {
+    commands.concat("A ");
+    Serial.println(commands);
+    delay(200);
+  }  
+  
+  if (digitalRead(BTN_D)) {
+    commands.concat("D ");
+    Serial.println(commands);
+    delay(200);
+  }    
+      
+  if (digitalRead(BTN_E)) {    
+     executeCommands();     
+  }      
+}
+
+void executeCommands(){  
+  while (commands.length() > 0)
+  {
+    int index = commands.indexOf(' ');
+    {
+      String currentCommand = commands.substring(0, index);      
+      commands = commands.substring(index+1);
+      
+      execute(currentCommand);
+      
+      delay(1000);
+    }
+  }
+  commands = "";
+}
+
+
+void execute(String command) {
+    String aComa = command.substring(0,1);
+  
+    Serial.println(aComa);
+    
+    if (aComa == "W") {
+      Frente();
+      delay(500);
+      Parar();
+    }
+
+    if (aComa == "D"){
+      Direita();
+      delay(500);
+      Parar();
+    }
+
+    if (aComa == "A"){
+      Esquerda();
+      delay(500);
+      Parar();
+    }    
+    
+}
+void Frente(){
+  
+  MotorEsquerda.setSpeed(255);
+  MotorDireita.setSpeed(255);
+  MotorEsquerda.run(FORWARD); 
+  MotorDireita.run(FORWARD);  
+}
+
+void Direita(){
+  MotorEsquerda.setSpeed(255);  
+  MotorEsquerda.run(FORWARD); 
+  MotorDireita.run(RELEASE);   
+}
+
+void Esquerda(){
+  MotorDireita.setSpeed(255);  
+  MotorDireita.run(FORWARD); 
+  MotorEsquerda.run(RELEASE);
+
+}
+
+void Parar(){ 
+  
+  MotorEsquerda.run(RELEASE);  
+  MotorDireita.run(RELEASE); 
+
+}
+  // continuar o código
